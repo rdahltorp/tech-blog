5c8980e5d42286d39991aa6d42f9bc2f93dd4502
rdahltorp <109421585+rdahltorp@users.noreply.github.com>
2022-11-19 10:50:23 -0700

    Added comment seeds to test functionality.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,9 @@
 const sequelize = require('../config/connection')
-const { User, Post } = require('../models')
+const { User, Post, Comment } = require('../models')
 
 const userData = require('./userData.json')
 const postData = require('./postData.json')
-//const commentData = require('./commentData.json')
+const commentData = require('./commentData.json')
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -16,6 +16,12 @@ const seedDatabase = async () => {
     for (const post of postData) {
         await Post.create({
             ...post,
+        })
+    }
+
+    for (comment of commentData) {
+        await Comment.create({
+            ...comment,
         })
     }
 
